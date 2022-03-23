ea1820c521cd0cd8d2ac44720ab512d85d867180
Thibault <thibaultfoure@gmail.com>
2022-03-23 10:47:02 +0100

    dojo correction

---
+++ README.md
@@ -0,0 +1,15 @@
+# Dojo CSS - PHP
+
+## Etape 1 - CSS
+En t'aidant de la structure HTML/CSS fournie, reproduit le design des cards selon l'image ci-dessous. Tu dois uniquement toucher au fichier *style.css* à ce stade. On ne cherche pas un rendu *pixel perfect*
+
+![css dojo](cssDojo.png)
+
+## Etape 2 - Responsive
+Fais en sorte que les cards soient responsive en t'aidant de l'image ci-dessous. 
+
+![css dojo resonsive](cssDojoResponsive.png)
+
+## Etape 3 - PHP
+Renomme le fichier *index.html* en *index.php*. Lance un serveur PHP.
+Puis, en t'aidant du fichier *data.php* fourni, boucle sur les données pour rendre le site dynamique.
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php include 'data.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS Dojo</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>John Doe</h1>
+        <h2>Consultant in economics</h2>
+    </header>
+    <main>
+        <?php foreach ($data as $categories => $infos) : ?>
+            <section>
+                <h1><?= $categories ?></h1>
+                <div class="cards">
+                    <?php foreach ($infos as $info) : ?>
+                        <figure>
+                            <img src="assets/images/<?= $info['image'] ?>" alt="Highschool of Smalltown">
+                            <figcaption>
+                                <div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar3" viewBox="0 0 16 16">
+                                        <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z" />
+                                        <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                                    </svg><?= $info['date'] ?></div>
+                                <h2><?= $info['title'] ?></h2>
+                                <p><?= $info['description'] ?></p>
+                                <div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bank" viewBox="0 0 16 16">
+                                        <path d="M8 .95 14.61 4h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.379l.5 2A.5.5 0 0 1 15.5 17H.5a.5.5 0 0 1-.485-.621l.5-2A.5.5 0 0 1 1 14V7H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 4h.89L8 .95zM3.776 4h8.447L8 2.05 3.776 4zM2 7v7h1V7H2zm2 0v7h2.5V7H4zm3.5 0v7h1V7h-1zm2 0v7H12V7H9.5zM13 7v7h1V7h-1zm2-1V5H1v1h14zm-.39 9H1.39l-.25 1h13.72l-.25-1z" />
+                                    </svg><?= $info['location'] ?></div>
+                            </figcaption>
+                        </figure>
+                    <?php endforeach ?>
+                </div>
+            </section>
+        <?php endforeach ?>
+    </main>
+</body>
+
+</html>
